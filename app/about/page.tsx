9df0d8dffc585e65ebf8a358e1e74e0445db8ee8
gabialonso <gabrielalonsomauro@gmail.com>
2025-11-30 16:04:11 -0300

    feat: add about, projects and contact pages

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import Hero from "@/components/Hero"
+
+export default function About() {
+  return (
+    <main>
+      <section>
+        <h1>About</h1>
+        <p>Biografía</p>
+      </section>
+    </main>
+  )
+}
